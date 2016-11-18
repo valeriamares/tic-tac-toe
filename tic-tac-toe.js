a59061575c00c9ad5f13ffde44f52451c35101a5
valeria <yoselin.perez13@gmail.com>
2016-11-18 11:06:21 -0800

    finished

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,146 @@
-// Write all your JavaScript code in this file!
-// You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var turn = "player1"
+function doSomethingPatricia() {
+   var newShape = document.createElementNS(namespace, "circle")
+ newShape.setAttribute("cx", 60)
+ newShape.setAttribute("cy", 50)
+ newShape.setAttribute("r", 30)
+ if (turn == "player1"){
+   newShape.setAttribute("fill", "skyblue")
+   turn = "player2"
+ }else{
+   newShape.setAttribute("fill", "yellow")
+   turn = "player1"
+ }
+ var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPatrici() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 60)
+newShape.setAttribute("cy", 145)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "yellow")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPatric() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 60)
+newShape.setAttribute("cy", 240)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "yellow")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPatri() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 155)
+newShape.setAttribute("cy", 50)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "yellow")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPatr() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 155)
+newShape.setAttribute("cy", 145)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "yellow")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomething() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 155)
+newShape.setAttribute("cy", 240)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "yellow")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPat() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 245)
+newShape.setAttribute("cy", 240)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "yellow")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+function doSomethingPao() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 245)
+newShape.setAttribute("cy", 50)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "yellow")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
+
+
+function doSomethingP() {
+  var newShape = document.createElementNS(namespace, "circle")
+newShape.setAttribute("cx", 245)
+newShape.setAttribute("cy", 145)
+newShape.setAttribute("r", 30)
+if (turn == "player1"){
+  newShape.setAttribute("fill", "skyblue")
+  turn = "player2"
+}else{
+  newShape.setAttribute("fill", "yellow")
+  turn = "player1"
+}
+var canvas = document.getElementById("game-board")
+canvas.appendChild(newShape)
+}
